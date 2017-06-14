71aadf69b22bdfe31837c0024aa69295136ea502
Eric Yang <ericswyang@gmail.com>
2017-06-13 22:11:06 -0400

    change favicon name, update intro message

--- src/components/IntroDeploy.jsx
+++ src/components/IntroDeploy.jsx
@@ -60,6 +60,9 @@ class IntroDeployContainer extends Component {
         <div>
           <div>
             <div className="container-vflex">
+              <div className="text-font">
+                My name is Eric, I am a software developer and I created this site using React w/ Redux!
+              </div>
               <div className="text-font">"Hi Eric, my name is <input className="text-font" type="text" autoComplete="off" name="name" placeholder="Name Please..." onChange={this.handleChange} onKeyDown={this.handleKeyDown} />
               "</div>
           </div>
